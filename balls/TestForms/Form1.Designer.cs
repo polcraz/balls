248a782f2edfbf5ebae198f9c622b03364d0c9ab
polcraz <polcrazpolcraz@gmail.com>
2018-09-25 22:27:43 +0300

    Add files via upload

---
+++ balls/TestForms/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TestForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(120, 13);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 47);
+            this.btnDraw.TabIndex = 2;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(415, 355);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ balls/TestForms/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestForms
+{
+    public partial class Form1 : Form
+    {
+        Graphics gr;
+        Random rnd;
+        struct Colors
+        {
+            public int r;
+            public int g;
+            public int b;
+        };
+        Colors[,] colors;
+        public Form1()
+        {
+            rnd = new Random();
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            int width = 50;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            btnDraw.Visible = false;
+            gr = Graphics.FromHwnd(Handle);
+            int x = 1, y = 1;
+            colors = new Colors[Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)]; 
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+            {
+                for (int j = 0; j < Convert.ToInt32(textBox2.Text); j++)
+                {
+                    colors[i, j].r = rnd.Next(0, 255);
+                    colors[i, j].g = rnd.Next(0, 255);
+                    colors[i, j].b = rnd.Next(0, 255);
+                    gr.DrawEllipse(new Pen(Color.FromArgb(colors[i, j].r, colors[i, j].g, colors[i, j].b)), x, y, this.ClientSize.Width / Convert.ToInt32(textBox2.Text) - 3, this.ClientSize.Height / Convert.ToInt32(textBox1.Text) - 2);
+                    x += this.ClientSize.Width / Convert.ToInt32(textBox2.Text);
+                }
+                x = 1;
+                y += this.ClientSize.Height / Convert.ToInt32(textBox1.Text);
+            }
+        }
+
+        private void Paint1(int n, int m)
+        {
+            gr = Graphics.FromHwnd(Handle);
+            int x = 1, y = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    gr.DrawEllipse(new Pen(Color.FromArgb(colors[i, j].r, colors[i, j].g, colors[i, j].b)), x, y, this.ClientSize.Width / m - 3, this.ClientSize.Height / n - 2);
+                    x += this.ClientSize.Width / m;
+                }
+                x = 1;
+                y += this.ClientSize.Height / n;
+            }
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            gr.Clear(Color.White);
+            Paint1(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            gr.Clear(Color.White);
+            textBox1.Visible = true;
+            textBox1.Text = "";
+            textBox2.Visible = true;
+            textBox2.Text = "";
+            btnDraw.Visible = true;
+        }
+    }
+}
